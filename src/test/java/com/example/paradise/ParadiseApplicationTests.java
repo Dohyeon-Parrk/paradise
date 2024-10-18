53476a9bb321be046a74ef8bf24c60862459cec2
shinheekim <shinhee1241@gmail.com>
2024-10-18 12:46:57 +0900

    init commit

---
+++ src/test/java/com/example/paradise/ParadiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paradise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParadiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
