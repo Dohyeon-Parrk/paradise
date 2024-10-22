0873e638d761dc071f98ec6eaa0bb668d32152b3
Dohyeon_Parrk <parkyj3213@naver.com>
2024-10-22 20:42:17 +0900

    feat : Comment 패키지 생성, Comment 기본 구조 완성

---
+++ src/main/java/com/example/paradise/domain/comment/api/CommentController.java
@@ -0,0 +1,12 @@
+package com.example.paradise.domain.comment.api;
+
+import com.example.paradise.domain.comment.application.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CommentController {
+
+    private final CommentService commentService;
+}
