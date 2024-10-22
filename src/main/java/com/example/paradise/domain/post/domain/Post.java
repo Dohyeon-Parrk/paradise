38ab7a09ef4c8770024f7a6fb5c9fe24e1fed974
JIMINJI1 <mj013031@gmail.com>
2024-10-22 17:22:21 +0900

    feat: Post 엔티티에 User와의 관계 추가 및 Timestamped 상속

--- src/main/java/com/example/paradise/domain/post/domain/Post.java
+++ src/main/java/com/example/paradise/domain/post/domain/Post.java
@@ -1,68 +1,47 @@
 package com.example.paradise.domain.post.domain;
 
+import com.example.paradise.common.Timestamped;
+import com.example.paradise.user.entity.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 @Entity
 @NoArgsConstructor
 @Table(name = "post")
-public class Post {
+public class Post extends Timestamped {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "post_id", nullable = false,updatable = false)
+    @Column(name = "post_id", nullable = false, updatable = false)
     private Long postId;
 
 
-    @Column(name = "content",nullable = false)
+    @Column(name = "content", nullable = false)
     private String content;
 
     @Column(name = "username", nullable = false)
     private String username;
 
 
-    @CreationTimestamp
-    @Column(name = "created_at",nullable = false,updatable = false)
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    @Column(name = "updated_at",nullable = false)
-    private LocalDateTime updatedAt;
-
-    // User 테이블이 있을 떄 사용
     //게시글 - 유저
-    //    @ManyToOne(fetch = FetchType.LAZY)
-    //    @JoinColumn(name="user_id")
-    //    private User user;
-    //
-    //    public Post(String content, User user) {
-    //        this.content = content;
-    //        this.user = user;
-    //    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 
-    // user 테이블이 없다고 할 때
-    private Long userId;
-    public Post(Long userId,String username, String content) {
-        this.userId = userId;
-        this.username = username;
+    public Post(User user, String content) {
+        this.username = user.getUsername();
         this.content = content;
     }
-
 
     // 게시글 업데이트 메서드
     public void updateContent(String content) {
         this.content = content;
 
     }
-
 
 
 }
